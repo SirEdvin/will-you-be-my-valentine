43559845663e270233161320d99635c8d0cd2d0f
SirEdvin <siredvin.dark@gmail.com>
2024-02-13 22:45:42 +0200

    Localization

--- src/App.tsx
+++ src/App.tsx
@@ -12,22 +12,20 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "What if I asked really nicely?",
-      "Pretty please",
-      "With a chocolate rice cake on top",
-      "What about a matcha frostie",
-      "PLEASE POOKIE",
-      "But :*(",
-      "I am going to die",
-      "Yep im dead",
-      "ok ur talking to nathan's ghost",
-      "please babe",
-      ":((((",
-      "PRETTY PLEASE",
-      "Estoy muerto",
-      "No :(",
+      "Ні",
+      "Ти певна?",
+      "Правда-правда певна?",
+      "Подумай знову!",
+      "Останній шанс!",
+      "Точно ні?",
+      "Можливо, це велика помилка!",
+      "Подумай про це ще раз!",
+      "Ти абсолютно точно певна?",
+      "Май бога в серці!",
+      "Може передумаєш?",
+      "Може краще вип'ємо смачної кавусі?",
+      "Це твоя остаточна відповідь?",
+      "Ти розбиваєш мені серце ;(",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -38,7 +36,7 @@ export default function Page() {
       {yesPressed ? (
         <>
           <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
-          <div className="my-4 text-4xl font-bold">WOOOOOO!!! I love you pookie!! ;))</div>
+          <div className="my-4 text-4xl font-bold">ЮХУУУ!!! Я тебе обожнюю, кохана!! ;))</div>
         </>
       ) : (
         <>
@@ -46,20 +44,20 @@ export default function Page() {
             className="h-[200px]"
             src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
           />
-          <h1 className="my-4 text-4xl">Will you be my Valentine?</h1>
+          <h1 className="my-4 text-4xl">Будеш моєї валентинкою?</h1>
           <div className="flex items-center">
             <button
               className={`mr-4 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700`}
               style={{ fontSize: yesButtonSize }}
               onClick={() => setYesPressed(true)}
             >
-              Yes
+              Так
             </button>
             <button
               onClick={handleNoClick}
               className=" rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-red-700"
             >
-              {noCount === 0 ? "No" : getNoButtonText()}
+              {noCount === 0 ? "Ні" : getNoButtonText()}
             </button>
           </div>
         </>
